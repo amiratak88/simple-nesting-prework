0efcbe4ce6d4622a35cec0fd9045c10672b82284
Amirata K <amiratak88@gmail.com>
2018-07-07 05:59:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -95,7 +95,10 @@ def adding_matz
         }
      }
 
-   programmer_hash[:yukihiro_matsumoto] = 
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP, C"]
+  }
     
     
     programmer_hash
